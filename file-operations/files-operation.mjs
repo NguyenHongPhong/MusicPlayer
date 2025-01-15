5365d84fc00f8dc03e91ad7e4e5b310f13174ac9
Po Lap Trinh <phongnguyenwn123@gmail.com>
2025-01-15 17:17:58 +0700

    Update code

--- file-operations/files-operation.mjs
+++ file-operations/files-operation.mjs
@@ -1,7 +1,12 @@
 import fs from 'fs';
 import multer from 'multer';
 import express from 'express';
-import path, { resolve } from 'path';
+import path from 'path';
+import morgan from 'morgan';
+import os from 'os';
+
+
+
 
 
 const resourcePath = '/public';
@@ -10,10 +15,27 @@ const localFolder = path.join(__dirname, './public/assets/FilesUploaded');
 const app = express();
 const port = 3000;
 
+//Cấu hình Morgan để kiểm tra xem request đã gửi lên server chưa
+app.use(morgan('combined'));
+
 // Cấu hình Express để sử dụng resource
 app.use(express.static(path.join(__dirname + resourcePath)));
 // Cấu hình Express để phục vụ tệp tĩnh từ thư mục FilesUploaded
 app.use('/FilesUploaded', express.static(path.join(__dirname, './public/assets/FilesUploaded')));
+
+// Cấu hình vô  hiệu hóa cache
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store');
+  next();
+});
+
+setInterval(() => {
+  console.log('Total Memory: ' + os.totalmem() / (1024 * 1024) + ' MB');
+  console.log('Free Memory: ' + os.freemem() / (1024 * 1024) + ' MB');
+  const used = process.memoryUsage().heapUsed / 1024 / 1024;
+console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+}, 5000);
+
 
 const fileUploadHandler = {
   
@@ -49,6 +71,7 @@ const fileUploadHandler = {
     }   
   },
 
+
   setup: function(imgPath, musPath, app) {    
     const storage = multer.diskStorage({
       destination: function(req, file, cb) {
@@ -63,28 +86,35 @@ const fileUploadHandler = {
       }
     });
 
-   
-    const upload = multer({ storage: storage });
+   const filer = (req, file, cb) => {
+     var ext = path.extname(file.originalname);
+      if( ext == '.jpeg' || ext === '.png' ||  ext === '.jpg' ) {
+        cb(null, true) ;
+      } else if (ext == '.mp3') {
+        cb(null, true);
+      } else {
+        cb(new Error('Allowed only AUDIO and IMAGE files !!!'), false)
+      }
+   };
 
-    app.get('/', function(req, res) {
-      console.log(__dirname);
-      
+    const upload = multer({ storage: storage, fileFilter: filer });
+
+    app.get('/', function(req, res) {    
       res.sendFile(path.join(__dirname, './index.html'));  // Đường dẫn tuyệt đối
     });
+
+   
 
     // Định nghĩa endpoint /upload với POST
     app.post('/upload',
       upload.fields([{ name: 'image-file', maxCount: 1 }, { name: 'music-file', maxCount: 1 }]), // Nhận tệp ảnh
-      (req, res) => {
+     async (req, res, next) => {
         try {
           if (req.files) {
             const imageFile = req.files['image-file'] ? req.files['image-file'][0].filename : null;
             const musicFile = req.files['music-file'] ? req.files['music-file'][0].filename : null;
 
-            console.log('Image file:', imageFile);
-            console.log('Music file:', musicFile);
-
-            res.json({
+            res.status(201).json({
               message: 'Files uploaded successfully',
               image: imageFile, // Tên tệp 1
               music: musicFile  // Tên tệp 2          
@@ -97,14 +127,22 @@ const fileUploadHandler = {
           }
         } catch (error) {
           console.error('Server error:', error);
-          res.status(500).json({
-            message: 'Internal Server Error',
-            error: error.message
-          });
+          next(error);
+          // res.status(500).json({
+          //   message: 'Internal Server Error',
+          //   error: error.message
+          // });
         }
       });
 
+      app.use((err, req, res, next)=> {
+           if(err) {
+           return res.status(400).json({message: err.message});
+         }
+       
+      })
   },
+  
 
   start: async function() {
     try {
